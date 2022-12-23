fd8e3ff6eeea04c15a2b5f37087c906f451be122
rirubelhossain <rubelphp94@gmail.com>
2022-12-24 02:00:44 +0600

    Add brand

---
+++ classes/Brand.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once '../lib/Database.php' ;
+include_once '../helpers/Format.php' ;
+
+?>
+
+<?php
+class Brand
+{
+    private $db ;
+    private $fm ;
+    
+    public function __construct(){
+        $this->db = new Database() ;
+        $this->fm = new Format() ;
+
+    }
+    public function brandInsert($brandname){
+        $brandname = $this->fm->validation($brandname) ;
+        $brandname = mysqli_real_escape_string($this->db->link,$brandname ) ;
+
+        if(empty($brandname)){
+            $insetmsg = "<span class = 'error'>Brand field must not be empty !</span>";
+            return $insetmsg ;
+        }else{
+            $query = "INSERT INTO tbl_brand(brandName) VALUES('$brandname')";
+            $result = $this->db->insert($query);
+
+            if( $result ){
+                $msg = "<span class = 'success'> Brand Inserted Successfully.</span>";
+                return $msg ;
+            }else{
+                $msg = "<span class = 'error'>Brand Inserted Not Successfully.</span>";
+                return $msg ;
+            }
+        }
+    }
+}
+
+
+
+?>
